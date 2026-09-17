4748532508121f91f0f209cbcf1e6626dfd7ec94
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-09-17 16:45:06 -0400

    fft plotted

--- fft/fft.py
+++ fft/fft.py
@@ -6,14 +6,22 @@ import matplotlib.pyplot as plt
 
 # Sine Wave at 0.15 Hz
 # Sample rate of 1Hz
-t = np.arange(100)
+Fs = 1 # Hz
+N = 100 # number of points to simulate, and our FFT size
+
+t = np.arange(N)
 s = np.sin(0.15*2*np.pi*t)
 
-S = np.fft.fft(s)
+# We want 0 Hz (DC) to be the center, that way negative frequencies are to the left
+# We need to perform an FFT shift
+S = np.fft.fftshift(np.fft.fft(s))
 
 # Calculate the magnitude & phase
 S_mag = np.abs(S)
 S_phase = np.angle(S)
-plt.plot(t, S_mag, '.-')
-plt.plot(t, S_phase, '.-')
+f = np.arange(Fs/-2, Fs/2, Fs/N)
+plt.figure(0)
+plt.plot(f, S_mag, '.-')
+plt.figure(1)
+plt.plot(f, S_phase, '.-')
 plt.show()
