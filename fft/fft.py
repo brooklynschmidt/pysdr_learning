fc91ce7c507c3c6b00f9c40cd4c2bd69ff6df6cf
brooklynschmidt <schmidt.bro@northeastern.edu>
2026-09-17 16:39:50 -0400

    fft on a sine wave

--- fft/fft.py
+++ fft/fft.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Creating a signal in the time domain
+
+# Sine Wave at 0.15 Hz
+# Sample rate of 1Hz
+t = np.arange(100)
+s = np.sin(0.15*2*np.pi*t)
+
+S = np.fft.fft(s)
+
+# Calculate the magnitude & phase
+S_mag = np.abs(S)
+S_phase = np.angle(S)
+plt.plot(t, S_mag, '.-')
+plt.plot(t, S_phase, '.-')
+plt.show()
